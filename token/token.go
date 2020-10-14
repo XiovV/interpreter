36bbfe5dc687c6e87a4c3fd13ccc35ce502a30c5
xiovv <xiovv@tutanota.com>
2020-10-14 16:19:27 +0200

    completed basic lexer

--- token/token.go
+++ token/token.go
@@ -11,6 +11,15 @@ const (
 
 	ASSIGN = "="
 	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
 
 	COMMA = ","
 	SEMICOLON = ";"
@@ -22,9 +31,31 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
 
 type Token struct {
 	Type TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
