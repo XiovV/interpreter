4241cb06ccfa098d213acff0891c58163ae49f28
XiovV <xiovv@tutanota.com>
2021-04-30 11:06:47 +0200

    extended REPL to use the parser

--- repl/repl.go
+++ repl/repl.go
@@ -4,12 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/XiovV/interpreter/lexer"
-	"github.com/XiovV/interpreter/token"
+	"github.com/XiovV/interpreter/parser"
 	"io"
 )
 
 const PROMPT = ">> "
-
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -23,9 +22,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
